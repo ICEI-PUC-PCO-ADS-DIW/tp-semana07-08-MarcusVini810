cb5b93269421afb2e477e2a2843c817473890640
Marcus Vinícius <marcus.vinicius@sga.pucminas.br>
2025-10-01 20:12:24 -0300

    Criação do Json dos cards

---
+++ public/app.js
@@ -0,0 +1,38 @@
+let cards = [
+    {
+        "id": 1,
+        "anime": "One Piece",
+        "resumo": "Luffy navega com amigos buscando liberdade; desafiam Governo Mundial, Século Perdido e a Vontade do D, buscando One Piece.",
+        "imagem": "img/OnePiece2.jpg",
+    },
+    {
+        "id": 2,
+        "anime":"Hunter X Hunter",
+        "resumo": "Gon viaja em busca do pai, enfrenta perigos, amizade, dilemas morais, explorando ambição, poder e a essência da humanidade.",
+        "imagem": "img/HxH.jpg",
+    },
+    {
+        "id": 3,
+        "anime": "Demon Slayer",
+        "resumo": "Tanjiro luta contra demônios após tragédia familiar, buscando cura para Nezuko, explorando dor, esperança, sacrifício e a fragilidade da vida.",
+        "imagem": "img/DemonSlayer.jpg",
+    },
+    {
+        "id": 4,
+        "anime": "Tokyo Ghoul",
+        "resumo": "Kaneki, após virar meio-ghoul, enfrenta identidade fragmentada, violência, preconceito e a dolorosa busca por aceitação e humanidade.",
+        "imagem": "img/TokyoGhoul.webp",
+    },
+    {
+        "id": 5,
+        "anime": "JoJo",
+        "resumo": "Família Joestar enfrenta gerações de inimigos sobrenaturais, explorando destino, coragem, poder e laços que transcendem tempo e legado.",
+        "imagem": "img/JoJo.png",
+    },
+    {
+        "id": 6,
+        "anime": "Black Clover",
+        "resumo": "Asta, sem magia, busca tornar-se Rei Mago; enfrenta desigualdade, demônios e rivalidades, mostrando perseverança, amizade e superação do impossível.",
+        "imagem": "img/BlackClover.jpg",
+    }
+]
